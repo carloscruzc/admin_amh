ab3063b57cdb321228b8071a307a4a357fca5968
Gerson Velasco <gvelassco@gmail.com>
2022-07-08 13:38:21 -0500

    1

--- backend/App/controllers/Principal.php
+++ backend/App/controllers/Principal.php
@@ -31,38 +31,38 @@ class Principal extends Controller{
      
 html;
 
-      $permisoGlobalHidden = (Controller::getPermisoGlobalUsuario($this->__usuario)[0]['permisos_globales']) != 1 ? "style=\"display:none;\"" : "";
-      $asistentesHidden = (Controller::getPermisosUsuario($this->__usuario, "seccion_asistentes", 1)==0)? "style=\"display:none;\"" : "";  
-      $vuelosHidden = (Controller::getPermisosUsuario($this->__usuario, "seccion_vuelos", 1)==0)? "style=\"display:none;\"" : "";  
-      $pickUpHidden = (Controller::getPermisosUsuario($this->__usuario, "seccion_pickup", 1)==0)? "style=\"display:none;\"" : "";
-      $habitacionesHidden = (Controller::getPermisosUsuario($this->__usuario, "seccion_habitaciones", 1)==0)? "style=\"display:none;\"" : ""; 
-      $cenasHidden = (Controller::getPermisosUsuario($this->__usuario, "seccion_cenas", 1)==0)? "style=\"display:none;\"" : ""; 
-      $cenasHidden = (Controller::getPermisosUsuario($this->__usuario, "seccion_cenas", 1)==0)? "style=\"display:none;\"" : ""; 
-      $aistenciasHidden = (Controller::getPermisosUsuario($this->__usuario, "seccion_asistencias", 1)==0)? "style=\"display:none;\"" : ""; 
-      $vacunacionHidden = (Controller::getPermisosUsuario($this->__usuario, "seccion_vacunacion", 1)==0)? "style=\"display:none;\"" : ""; 
-      $pruebasHidden = (Controller::getPermisosUsuario($this->__usuario, "seccion_pruebas_covid", 1)==0)? "style=\"display:none;\"" : "";
-      $configuracionHidden = (Controller::getPermisosUsuario($this->__usuario, "seccion_configuracion", 1)==0)? "style=\"display:none;\"" : "";
-      $utileriasHidden = (Controller::getPermisosUsuario($this->__usuario, "seccion_utilerias", 1)==0)? "style=\"display:none;\"" : "";  
+    //   $permisoGlobalHidden = (Controller::getPermisoGlobalUsuario($this->__usuario)[0]['permisos_globales']) != 1 ? "style=\"display:none;\"" : "";
+    //   $asistentesHidden = (Controller::getPermisosUsuario($this->__usuario, "seccion_asistentes", 1)==0)? "style=\"display:none;\"" : "";  
+    //   $vuelosHidden = (Controller::getPermisosUsuario($this->__usuario, "seccion_vuelos", 1)==0)? "style=\"display:none;\"" : "";  
+    //   $pickUpHidden = (Controller::getPermisosUsuario($this->__usuario, "seccion_pickup", 1)==0)? "style=\"display:none;\"" : "";
+    //   $habitacionesHidden = (Controller::getPermisosUsuario($this->__usuario, "seccion_habitaciones", 1)==0)? "style=\"display:none;\"" : ""; 
+    //   $cenasHidden = (Controller::getPermisosUsuario($this->__usuario, "seccion_cenas", 1)==0)? "style=\"display:none;\"" : ""; 
+    //   $cenasHidden = (Controller::getPermisosUsuario($this->__usuario, "seccion_cenas", 1)==0)? "style=\"display:none;\"" : ""; 
+    //   $aistenciasHidden = (Controller::getPermisosUsuario($this->__usuario, "seccion_asistencias", 1)==0)? "style=\"display:none;\"" : ""; 
+    //   $vacunacionHidden = (Controller::getPermisosUsuario($this->__usuario, "seccion_vacunacion", 1)==0)? "style=\"display:none;\"" : ""; 
+    //   $pruebasHidden = (Controller::getPermisosUsuario($this->__usuario, "seccion_pruebas_covid", 1)==0)? "style=\"display:none;\"" : "";
+    //   $configuracionHidden = (Controller::getPermisosUsuario($this->__usuario, "seccion_configuracion", 1)==0)? "style=\"display:none;\"" : "";
+    //   $utileriasHidden = (Controller::getPermisosUsuario($this->__usuario, "seccion_utilerias", 1)==0)? "style=\"display:none;\"" : "";  
 
-      $all_ra = AsistentesDao::getAllRegistrosAcceso();
-    //   $this->setTicketVirtual($all_ra);
-      $this->setClaveRA($all_ra);
+    //   $all_ra = AsistentesDao::getAllRegistrosAcceso();
+    // //   $this->setTicketVirtual($all_ra);
+    //   $this->setClaveRA($all_ra);
 
-      View::set('permisoGlobalHidden',$permisoGlobalHidden);
-      View::set('asistentesHidden',$asistentesHidden);
-      View::set('vuelosHidden',$vuelosHidden);
-      View::set('pickUpHidden',$pickUpHidden);
-      View::set('habitacionesHidden',$habitacionesHidden);
-      View::set('cenasHidden',$cenasHidden);
-      View::set('aistenciasHidden',$aistenciasHidden);
-      View::set('vacunacionHidden',$vacunacionHidden);
-      View::set('pruebasHidden',$pruebasHidden);
-      View::set('configuracionHidden',$configuracionHidden);
-      View::set('utileriasHidden',$utileriasHidden);
+    //   View::set('permisoGlobalHidden',$permisoGlobalHidden);
+    //   View::set('asistentesHidden',$asistentesHidden);
+    //   View::set('vuelosHidden',$vuelosHidden);
+    //   View::set('pickUpHidden',$pickUpHidden);
+    //   View::set('habitacionesHidden',$habitacionesHidden);
+    //   View::set('cenasHidden',$cenasHidden);
+    //   View::set('aistenciasHidden',$aistenciasHidden);
+    //   View::set('vacunacionHidden',$vacunacionHidden);
+    //   View::set('pruebasHidden',$pruebasHidden);
+    //   View::set('configuracionHidden',$configuracionHidden);
+    //   View::set('utileriasHidden',$utileriasHidden);
 
       View::set('asideMenu',$this->_contenedor->asideMenu());
       View::set('header',$this->_contenedor->header($extraHeader));
-      View::set('footer',$this->_contenedor->footer($extraFooter));
+    //   View::set('footer',$this->_contenedor->footer($extraFooter));
       View::set('permiso_caja',Controller::getPermisoUser($this->__usuario)['menu_caja']);
       View::render("principal_all");
     }
